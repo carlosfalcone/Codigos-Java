82372c9c55dabbbd70d0ecfda709fcd5aeca8bbc
Carlos Falcone <carloseduardo.falcone@gmail.com>
2023-05-20 15:27:07 -0300

    EstudosDia20Maio

---
+++ AnatomiaDasClasses/src/Teste2.java
@@ -0,0 +1,12 @@
+public class Teste2 {
+    
+    public static void main(String[] args) {
+        int variavel = 9; // nao tem como deixar a variavel sem a definiçao de valor
+        System.out.println(variavel);
+
+        final double PI = 3.14; // palavra reservada 'final' defini uma variavel com um unico valor constante
+        System.out.println(PI);
+        PI = 4.2;
+    }
+
+}
